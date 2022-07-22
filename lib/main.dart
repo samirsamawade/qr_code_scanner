f84ee020b7e561361f183544644ba7b8eab09db5
samir <samirsamawade@gmail.com>
2022-07-22 22:10:05 +0300

    Title Name

--- lib/main.dart
+++ lib/main.dart
@@ -27,7 +27,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'QR Code Scanner',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
