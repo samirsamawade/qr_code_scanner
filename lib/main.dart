4121d3a5bc1082671be8cb81522e447e3eb8f7eb
samir <samirsamawade@gmail.com>
2022-07-24 21:31:17 +0300

    Hide Debug Mode Banner

--- lib/main.dart
+++ lib/main.dart
@@ -29,6 +29,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'QR Code Scanner',
       theme: ThemeData(
         primarySwatch: Colors.blue,
