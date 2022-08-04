b5274872490e044ffff0c6d4774207e4c8a988fa
samir <samirsamawade@gmail.com>
2022-08-04 16:31:28 +0300

    App Icons

--- lib/main.dart
+++ lib/main.dart
@@ -9,21 +9,6 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  static const customSwatch = MaterialColor(
-    0xFFFF5252,
-    <int, Color>{
-      50: Color(0xFFFFEBEE),
-      100: Color(0xFFFFCDD2),
-      200: Color(0xFFEF9A9A),
-      300: Color(0xFFE57373),
-      400: Color(0xFFEF5350),
-      500: Color(0xFFFF5252),
-      600: Color(0xFFE53935),
-      700: Color(0xFFD32F2F),
-      800: Color(0xFFC62828),
-      900: Color(0xFFB71C1C),
-    },
-  );
 
   const MyApp({Key? key}) : super(key: key);
 
